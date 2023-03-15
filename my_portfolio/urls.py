85053175f892f50fecd274a445823be92b7c9cdf
gabriel <gabrielsantos98898@gmail.com>
2023-03-15 17:21:42 -0300

    Terminado ajustes na seção de experience

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about_me/', views.about, name="about-me")
 ]
--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'sections_portfolio.html', {})
+
+def about(request):
+    return render(request, 'about_me.html', {})
