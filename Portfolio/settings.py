096019f8e0863236f1541973ccdc7dfbaa6d6f85
Gabriel Santos <gabrielsantos98898@gmail.com>
2024-02-19 14:49:14 -0300

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = [os.getenv('SECRET_KEY'), get_random_secret_key()]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.getenv('DOMAIN'), '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
